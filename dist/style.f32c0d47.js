1c084b3c5cffe83717a3300d32319119e1d142cf
Maalxi <maalxi38@gmail.com>
2023-03-09 16:19:56 +0100

    commit du 9mars 2023

--- dist/style.f32c0d47.js
+++ dist/style.f32c0d47.js
@@ -172,7 +172,7 @@ module.exports = reloadCSS;
 var reloadCSS = require('_css_loader');
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"./../img/banner_bg.jpg":[["banner_bg.326cf322.jpg","inc/img/banner_bg.jpg"],"inc/img/banner_bg.jpg"],"_css_loader":"../../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"./../img/banner_bg.jpg":[["banner_bg.326cf322.jpg","inc/img/banner_bg.jpg"],"inc/img/banner_bg.jpg"],"./../img/about-bg.jpg":[["about-bg.3ca6e3b3.jpg","inc/img/about-bg.jpg"],"inc/img/about-bg.jpg"],"_css_loader":"../../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -197,7 +197,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "37747" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "38573" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
