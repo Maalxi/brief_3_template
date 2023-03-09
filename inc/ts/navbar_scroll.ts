b8fd212d69e4195688f8fb5072bd262484ef2d59
Maalxi <maalxi38@gmail.com>
2023-03-09 09:09:15 +0100

    Mise a jour du 9mars 2023

---
+++ inc/ts/navbar_scroll.ts
@@ -0,0 +1,52 @@
+window.addEventListener("scroll", function() {
+  const topNav = document.querySelector(".top-nav") as HTMLElement;
+  const scrollPosition = window.pageYOffset;
+
+  if (scrollPosition > 80) {
+    topNav.classList.add("top-nav-scroll");
+    topNav.style.backgroundColor = "white";
+    const menuItems = topNav.querySelectorAll(".menu li") as NodeListOf<HTMLElement>;
+    menuItems.forEach(item => {
+      item.style.color = "black";
+    });
+  } else {
+    topNav.classList.remove("top-nav-scroll");
+    topNav.style.backgroundColor = "transparent";
+    const menuItems = topNav.querySelectorAll(".menu li") as NodeListOf<HTMLElement>;
+    menuItems.forEach(item => {
+      item.style.color = "white";
+    });
+  }
+});
+
+
+// window.addEventListener("scroll", function() {
+//   const topNav = document.querySelector(".top-nav") as HTMLElement;
+//   const scrollPosition = window.pageYOffset;
+
+//   if (scrollPosition > 80) {
+//     topNav.classList.add("top-nav-scroll");
+//     const menuItems = topNav.querySelectorAll(".menu li") as NodeListOf<HTMLElement>;
+//     menuItems.forEach(item => {
+//       item.style.color = "black";
+//     });
+//   } else {
+//     topNav.classList.remove("top-nav-scroll");
+//     const menuItems = topNav.querySelectorAll(".menu li") as NodeListOf<HTMLElement>;
+//     menuItems.forEach(item => {
+//       item.style.color = "white";
+//     });
+//   }
+// });
+
+
+// window.addEventListener("scroll", function() {
+//   const topNav = document.querySelector(".top-nav") as HTMLElement;
+//   const scrollPosition = window.pageYOffset;
+
+//   if (scrollPosition > 80) {
+//     topNav.classList.add("top-nav-scroll");
+//   } else {
+//     topNav.classList.remove("top-nav-scroll");
+//   }
+// });
